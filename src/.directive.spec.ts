8fe6f0b7b4d768fd94e2fcbe23ffdb9754c1fdc8
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2023-05-21 16:36:22 +0530

    Initial commit

---
+++ src/.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CustomDirective } from './.directive';
+
+describe('Directive', () => {
+  it('should create an instance', () => {
+    const directive = new CustomDirective();
+    expect(directive).toBeTruthy();
+  });
+});
